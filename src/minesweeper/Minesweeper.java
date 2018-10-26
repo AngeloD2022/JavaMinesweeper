8b2310c8416cec440ba3280cecd1c8febbb68405
delucaa.2022 <delucaa.2022@KHS-C214-24767.kenston.local>
2018-10-26 12:37:07 -0400

    first commit

---
+++ src/minesweeper/Minesweeper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author delucaa.2022
+ */
+public class Minesweeper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Board b = new Board(20, 15, 9);
+        b.displayBoard();
+    }
+    
+}
